531af0a30619e41557011ff733d8fac3fd286089
Bruno3C <brunodossantos.job@gmail.com>
2022-11-10 19:56:53 -0300

    :heavy_plus_sign: build: add native-base

--- App.tsx
+++ App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
 
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View>
+    <NativeBaseProvider>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-      { fontsLoaded ? <Text>Open up App.js to start working on your app!</Text> : <View/> }
-    </View>
+      { fontsLoaded ? <View/> : <View/> }
+    </NativeBaseProvider>
   );
 }
