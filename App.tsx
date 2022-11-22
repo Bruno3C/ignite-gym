d5afb799fa83c9537d5deac107eab1a74d53b593
Bruno3C <brunodossantos.job@gmail.com>
2022-11-22 19:15:30 -0300

    :lipstick: feat: customizando o tema default

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import { StatusBar, View } from 'react-native';
-import { Loading } from '@components/Loading';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/theme';   
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
       { fontsLoaded ? <View/> : <Loading/> }
     </NativeBaseProvider>
